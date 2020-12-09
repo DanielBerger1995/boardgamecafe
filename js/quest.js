569145b92801222fe521103bc332424df9ec4b9d
oliverdotcom <55577972+oliverdotcom@users.noreply.github.com>
2020-12-09 15:37:14 +0100

    comments and fixes for tha quest

--- js/quest.js
+++ js/quest.js
@@ -3,25 +3,92 @@
 // ========== GLOBAL VARIABLES ========== //
 
 const _videoGameRef = _db.collection("videogames");
-let _videoGames;
+let _actionSolo;
+let _actionTeam;
+let _strategySolo;
+let _strategyTeam;
+let _roleplayingSolo;
+let _roleplayingTeam;
 
 
-//ACTION SHOOTER SOLO//
-_videoGameRef.where("genre", "==", "ass").onSnapshot(snapshotData => {
-    _videoGames = [];
+
+/* ACTION SOLO */
+
+_videoGameRef.where("genre", "==", "as").onSnapshot(snapshotData => {
+    _actionSolo = [];
     snapshotData.forEach(doc => {
         let videoGame = doc.data();
         videoGame.id = doc.id;
-        _videoGames.push(videoGame);
+        _actionSolo.push(videoGame);
+    });
+
+});
+
+/* ACTION TEAM */
+
+_videoGameRef.where("genre", "==", "at").onSnapshot(snapshotData => {
+    _actionTeam = [];
+    snapshotData.forEach(doc => {
+        let videoGame = doc.data();
+        videoGame.id = doc.id;
+        _actionTeam.push(videoGame);
+    });
+
+});
+
+/* ROLEPLAYING SOLO */
+
+_videoGameRef.where("genre", "==", "rs").onSnapshot(snapshotData => {
+    _roleplayingSolo = [];
+    snapshotData.forEach(doc => {
+        let videoGame = doc.data();
+        videoGame.id = doc.id;
+        _roleplayingSolo.push(videoGame);
+    });
+
+});
+
+/* ROLEPLAYING TEAM */
+
+_videoGameRef.where("genre", "==", "rt").onSnapshot(snapshotData => {
+    _roleplayingTeam = [];
+    snapshotData.forEach(doc => {
+        let videoGame = doc.data();
+        videoGame.id = doc.id;
+        _roleplayingTeam.push(videoGame);
+    });
+
+});
+
+/* STRATEGY SOLO */
+
+_videoGameRef.where("genre", "==", "rs").onSnapshot(snapshotData => {
+    _strategySolo = [];
+    snapshotData.forEach(doc => {
+        let videoGame = doc.data();
+        videoGame.id = doc.id;
+        _strategySolo.push(videoGame);
+    });
+
+});
+
+/* STRATEGY TEAM */
+
+_videoGameRef.where("genre", "==", "rt").onSnapshot(snapshotData => {
+    _strategyTeam = [];
+    snapshotData.forEach(doc => {
+        let videoGame = doc.data();
+        videoGame.id = doc.id;
+        _strategyTeam.push(videoGame);
     });
 
 });
 
 
-// append videoGames to the DOM
-function appendVideoGames(videoGames) {
+// append videoGames to the DOM (replace actionSolo with any genre)
+function appendVideoGames(actionSolo) {
     let htmlTemplate = "";
-    for (let videoGame of videoGames) {
+    for (let videoGame of actionSolo) {
         htmlTemplate += `
     <article>
       <img src="${videoGame.img}">
@@ -59,11 +126,11 @@ gsap.from(".line-one", {
 
 gsap.from(".circle-one", {
     scrollTrigger: {
-        trigger: "#sec2",
+        trigger: "#aBtns",
         start: "top center",
         end: "bottom center",
         toggleActions: "restart pause reverse pause",
-        markers: false,
+        markers: true,
         scrub: true
     },
     scaleY: 0,
@@ -108,10 +175,9 @@ gsap.from(".circle-two", {
 
 
 
-document.getElementById("scroll-btn").addEventListener("click", slowScroll);
-
-function slowScroll() {
-    gsap.to(window, { duration: 5, scrollTo: "#sec2" });
+function actionBtn() {
+    document.getElementById("sBtns").style.display = "none";
+    document.getElementById("rBtns").style.display = "none";
 }
 
 
@@ -120,6 +186,8 @@ function slowScroll() {
 /* LEFT AND RIGHT SIDE BUTTONS */
 
 function btn1aClick() {
+    document.getElementById("sBtns").style.display = "none";
+    document.getElementById("rBtns").style.display = "none";
     document.querySelectorAll('.btn1').forEach(el => el.classList.add('disappearing'));
     setTimeout(() => {
         document.querySelectorAll('.btn1').forEach(el => el.style.display = "none")
@@ -132,6 +200,8 @@ function btn1aClick() {
 }
 
 function btn1cClick() {
+    document.getElementById("aBtns").style.display = "none";
+    document.getElementById("sBtns").style.display = "none";
     document.querySelectorAll('.btn1').forEach(el => el.classList.add('disappearing'));
     setTimeout(() => {
         document.querySelectorAll('.btn1').forEach(el => el.style.display = "none")
@@ -173,6 +243,8 @@ function btn2bClick() {
 /* MIDDLE BUTTONS */
 
 function btn1bClick() {
+    document.getElementById("aBtns").style.display = "none";
+    document.getElementById("sBtns").style.display = "none";
     document.querySelectorAll('.btn1a, .btn1c').forEach(el => el.classList.add('disappearing'));
     setTimeout(() => {
         document.querySelectorAll('.btn1a, .btn1c').forEach(el => el.style.display = "none")
