c9125a87047dafd8cc868dc48c81b7d77af38217
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2020-12-10 11:42:22 +0100

    sign in and select added

--- js/booking.js
+++ js/booking.js
@@ -4,7 +4,10 @@ const loginBox = document.querySelector(".booking");
 buttonTrigger.addEventListener('click', () => {
      loginBox.style.display = "flex";
      loginBox.classList.add("active")
-})
+});
+
+let _firebaseUI;
+
 
 firebase.auth().onAuthStateChanged(function (user) {
      if (user) { // if user exists and is authenticated
@@ -14,55 +17,77 @@ firebase.auth().onAuthStateChanged(function (user) {
      }
    });
    
-   function userAuthenticated(user) {
-     appendUserData(user);
-     hideTabbar(false);
-     showLoader(false);
+   function userAuthenticated(){
+     hideLogin();
    }
-   
    function userNotAuthenticated() {
-     hideTabbar(true);
-     showPage("login");
    
      // Firebase UI configuration
      const uiConfig = {
        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
        signInOptions: [
          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-         firebase.auth.FacebookAuthProvider.PROVIDER_ID
+         
        ],
-       signInSuccessUrl: '#home'
+       signInFlow: 'popup',
+       callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            hideLogin();
+            return false;
+        }
+    },
      };
      // Init Firebase UI Authentication
      if (!_firebaseUI) {
        _firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
      }
      _firebaseUI.start('#firebaseui-auth-container', uiConfig);
-     showLoader(false);
    }
    
-   // show and hide tabbar
-   function hideTabbar(hide) {
-     let tabbar = document.querySelector('#tabbar');
-     if (hide) {
-       tabbar.classList.add("hide");
-     } else {
-       tabbar.classList.remove("hide");
-     }
+   function  hideLogin(){
+     let authContainer = document.querySelector('#firebaseui-auth-container');
+     authContainer.style.display = 'none';
    }
-   
    
    // sign out user
    function logout() {
      firebase.auth().signOut();
    }
-   
-   function appendUserData(user) {
-     console.log(user);
-     document.querySelector('#user-data').innerHTML = `
-       <img class="profile-img" src="${user.photoURL || "img/placeholder.jpg"}">
-       <h3>${user.displayName}</h3>
-       <p>${user.email}</p>
-     `;
-   }
+
+   const bookingButtons = document.querySelectorAll(".booking div button");
+   const bookingSegments = document.querySelectorAll(".booking>div");
+   const bookingBackButton = document.querySelector(".booking>div:nth-of-type(3) span a");
+   const contentBox = document.querySelectorAll(".booking>div .content");
+   const bookingForm = document.querySelectorAll(".booking>div form")
+
+   bookingForm[0].addEventListener('submit', (e) => {
+    e.preventDefault();
+   })
+  bookingForm[1].addEventListener('submit', (e) => {
+    e.preventDefault();
+   })
+
+bookingButtons[0].addEventListener('click', ()=>{
+  bookingSegments[1].classList.remove('active');
+  bookingSegments[2].classList.add('active');
+  contentBox[0].style.display = 'none'
+  bookingButtons[0].style.display = 'none'
+
+})
+bookingBackButton.addEventListener('click', ()=>{
+  bookingSegments[2].classList.remove('active');
+  bookingSegments[1].classList.add('active');
+  contentBox[0].style.display = 'flex';
+  bookingButtons[0].style.display = 'block'
+})
+
+
+const bookingSeats = document.querySelector('#seats');
+const bookingTimetable = document.querySelector('#timetable');
+const bookingCafes = document.querySelector('#cafes');
+const bookingDate = document.querySelector('#dates');
+
+console.log(bookingDate.value)
+
+
+
